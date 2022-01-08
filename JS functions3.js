95547804b9fdc3e12e75b76703065ef14f0e2941
Rexaon <93644372+Rexaon@users.noreply.github.com>
2022-01-08 17:36:45 +0100

    Add files via upload

---
+++ JS functions3.js
@@ -0,0 +1,13 @@
+var i, j, red, kolonne;
+rad=prompt("Skriv inn antall rader i tabellen");
+kolonne=prompt("Skriv inn antall kolonner i tabellen");
+
+document.write("<table border='1'>");
+for(i=1; 1<=rad; i++) {
+    document.write("<tr>");
+    for(j=1; j<=kolonne; j++) {
+        document.write("<td style=\"padding: 10px;\">Røyken VGS</tr>");
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
